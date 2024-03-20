2748925a7e5cfd760054b8280fe4153e87b7d659
Josh <joshuazieve2013@u.northwestern.edu>
2024-03-19 23:35:44 -0400

    updating script

--- read_temp.py
+++ read_temp.py
@@ -9,49 +9,43 @@ sensor = adafruit_dht.DHT11(board.D4)
 humidifier = digitalio.DigitalInOut(board.D17)
 humidifier.direction = digitalio.Direction.OUTPUT
 
-humidifier_on = False
-
 def press_button():
-    global humidifier_on  # Declare humidifier_on as global
     # this is the equivalent of pressing the button for ~.5 seconds
     humidifier.value = True
     time.sleep(.5)
-    # humidifier.value = False
+    humidifier.value = False
 
 def turn_on():
-    global humidifier_on  # Declare humidifier_on as global
-    # this is the equivalent of pressing the button once IF humidifier is not on
-    if not humidifier_on:
-        press_button()
-        humidifier_on = True
+    # this is the equivalent of pressing the button once
+    press_button()
 
 def turn_off():
-    global humidifier_on  # Declare humidifier_on as global
-    # this is the equivalent of pressing the button twice IF the humidifier is on
-    if humidifier_on:
-        press_button()
-        press_button()
-        humidifier_on = False
+    # this is the equivalent of pressing the button twice
+    press_button()
+    press_button()
 
-while True:
+max_attempts = 3
+attempts = 0
+
+while attempts < max_attempts:
     try:
         # Print the values to the serial port
         temperature_c = sensor.temperature
         temperature_f = temperature_c * (9 / 5) + 32
         humidity = sensor.humidity
         print("Temp={0:0.1f}ºC, Temp={1:0.1f}ºF, Humidity={2:0.1f}%".format(temperature_c, temperature_f, humidity))
-        if (humidity < 70):
-            print("humidity is less than 70")
-            turn_on()
-        else:
-            turn_off()
+        if humidity is not None:
+            if humidity < 70:
+                print("humidity is less than 70")
+                turn_on()
+                time.sleep(10)  # Keep humidifier on for 10 seconds
+                turn_off()
+            break
     except RuntimeError as error:
         # Errors happen fairly often, DHT's are hard to read, just keep going
         print(error.args[0])
-        time.sleep(2.0)
-        continue
-    except Exception as error:
-        sensor.exit()
-        raise error
+    attempts += 1
+    time.sleep(2.0)
 
-    time.sleep(3.0)
+if attempts == max_attempts:
+    print("Failed to get humidity after {} attempts".format(max_attempts))
